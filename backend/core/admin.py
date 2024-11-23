ac640ba6c1691ba179db5980c0f704c22b4476bd
Dejvinczi <dejvinczi.dev@gmail.com>
2024-11-23 17:10:00 +0100

    feat: Register Event and EventParticipant models to admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Event, EventParticipant
 
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
 
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+class EventParticipantAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventParticipant, EventParticipantAdmin)
