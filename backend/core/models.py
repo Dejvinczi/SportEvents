dea58750cd38bfe5e9bf60565bc0dcff4a5460d2
Dejvinczi <dejvinczi.dev@gmail.com>
2024-11-23 17:09:36 +0100

    feat: Create Event and EventParticipiant models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
@@ -5,3 +6,33 @@ class User(AbstractUser):
     """User database representation model."""
 
     pass
+
+
+class Event(models.Model):
+    """Event database representation model."""
+
+    class Status(models.TextChoices):
+        PLANNED = "planned", "Planned"
+        IN_PROGRESS = "in_progress", "In Progress"
+        COMPLETED = "completed", "Completed"
+
+    name = models.CharField(max_length=255)
+    start_at = models.DateTimeField()
+    location = models.TextField()
+    participant_limit = models.PositiveBigIntegerField()
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=11, choices=Status.choices, default=Status.PLANNED
+    )
+    participants = models.ManyToManyField(to=User, through="EventParticipant")
+
+    def __str__(self):
+        return f"{self.name}({self.start_at}) - {self.status}"
+
+
+class EventParticipant(models.Model):
+    """Event participant representation model."""
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
